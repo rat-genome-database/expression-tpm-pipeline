3509031e355ba224c8a40001fd973ea9fce1ddb8
llamersmcw <llamers@mcw.edu>
2025-05-07 07:37:06 -0500

    Update runComputedSexLoad.sh

--- src/main/dist/runComputedSexLoad.sh
+++ src/main/dist/runComputedSexLoad.sh
@@ -13,6 +13,6 @@ cd $APPDIR
 
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
-    -jar lib/$APPNAME.jar --loadComputedSex "$@" > run.log 2>&1
+    -jar lib/$APPNAME.jar --loadComputedSex "$@" > runCs.log 2>&1
 
-mailx -s "[$SERVER] expression-tpm-pipeline Computed Sex Run" $EMAILLIST < $APPDIR/logs/summary.log
+mailx -s "[$SERVER] expression-tpm-pipeline Computed Sex Run" $EMAILLIST < $APPDIR/logs/csSummary.log
